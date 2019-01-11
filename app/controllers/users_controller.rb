4e18c18cf5d05a4aa370c9c3f4b14ab34b8b7a1d
said shah <saidmysr20@aol.com>
2019-01-11 09:13:49 -0500

    added a create method which signs up user with jwt and returns user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController
     render json: User.all
   end
 
+  def create
+    @newUser = User.create(signUpParams)
+    if @newUser.valid?
+      token = JWT.encode({user_id: @newUser.id}, "SECRET")
+      render json: {user: @newUser, jwt:token}
+    else
+      render json: {error: "CANT CREATE USER"}, status: 422
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def signUpParams
+    params.require(:user).permit(:first_name, :last_name, :password, :age, :username)
+  end
+
 end
