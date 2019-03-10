81f87f00a0265b5e160b0dea96441a3e99a100d4
said shah <saidmysr20@aol.com>
2019-03-10 14:33:35 -0400

    update to deploy on heroku

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,15 +4,18 @@ class AuthController < ApplicationController
     user = User.find_by(username: userParams[:username])
     if user && user.authenticate(userParams[:password])
         token = JWT.encode({user_id: user.id,}, "SECRET")
+
         render json: {user: user, jwt: token}
       else
         render json: {user: {}}, status: 422
+
     end
   end
 
   def show
     user = User.find(JWT.decode(request.authorization, "SECRET")[0]["user_id"])
     render json: {user: user, jwt: request.authorization}
+    
   end
 
   private
